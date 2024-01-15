502d4547ff2c0d363015939eb436b2e13002a12e
l00pss <mammadli.vugar@hotmail.com>
2024-01-15 12:46:06 +0400

    first look

--- src/main/java/com/intellibucket/pipeql/view/client/main/abstracts/AbstractCenterPanelClient.java
+++ src/main/java/com/intellibucket/pipeql/view/client/main/abstracts/AbstractCenterPanelClient.java
@@ -1,5 +1,8 @@
 package com.intellibucket.pipeql.view.client.main.abstracts;
 
+import com.intellibucket.pipeql.eventlink.rx.abstracts.Callback;
+
 public interface AbstractCenterPanelClient {
     void closeAllSchemas();
+    void closeAllSchemas(Callback callback);
 }
--- src/main/java/com/intellibucket/pipeql/view/client/main/concretes/CenterPanelClient.java
+++ src/main/java/com/intellibucket/pipeql/view/client/main/concretes/CenterPanelClient.java
@@ -2,6 +2,7 @@ package com.intellibucket.pipeql.view.client.main.concretes;
 
 import com.intellibucket.pipeql.eventlink.model.common.Topic;
 import com.intellibucket.pipeql.eventlink.model.producer.ProducingMessage;
+import com.intellibucket.pipeql.eventlink.rx.abstracts.Callback;
 import com.intellibucket.pipeql.view.client.main.abstracts.AbstractCenterPanelClient;
 import com.intellibucket.pipeql.view.client.main.abstracts.DomainClient;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +16,23 @@ public class CenterPanelClient extends DomainClient implements AbstractCenterPan
 
     @Override
     public void closeAllSchemas() {
-        log.info("CenterPanelClient executing closeAllSchemas");
         var request = ProducingMessage.Builder
                 .builder()
                 .topic(Topics.CLOSE_ALL_SCHEMAS_TOPIC)
+                .build();
+        this.publish(request);
+    }
+
+    private void closeAllSchemas(ProducingMessage request){
+        log.info("CenterPanelClient executing closeAllSchemas");
+        this.publish(request);
+    }
+    @Override
+    public void closeAllSchemas(Callback callback) {
+        var request = ProducingMessage.Builder
+                .builder()
+                .topic(Topics.CLOSE_ALL_SCHEMAS_TOPIC)
+                .callback(callback)
                 .build();
         this.publish(request);
     }
