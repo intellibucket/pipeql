1439dbbd059614026171dc4add26a61f6c4f6cae
l00pss <mammadli.vugar@hotmail.com>
2024-01-23 10:59:16 +0400

    first look

--- src/main/java/com/intellibucket/pipeql/lib/panel/GListItemPanel.java
+++ src/main/java/com/intellibucket/pipeql/lib/panel/GListItemPanel.java
@@ -17,7 +17,7 @@ public class GListItemPanel extends AbstractGPanel{
     private final TableItemModel item;
     private final AbstractGLabel picLabel;
     private final AbstractGLabel label;
-    private final AbstractGButton dropButton = new SimpleIconGButton("green");
+    private final AbstractGButton dropButton = new SimpleIconGButton("inspectionsOK");
 
     {
         this.setLayout(new BorderLayout());
